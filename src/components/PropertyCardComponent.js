402d85eaac18787d1040c69cab99b3eb1ee04fec
Chris Peakin <christopher.peakin@googlemail.com>
2022-04-16 11:36:04 +0100

    updated styling

--- src/components/PropertyCardComponent.js
+++ src/components/PropertyCardComponent.js
@@ -6,22 +6,22 @@ function PropertyCard(props) {
   const { title, type, city, bathrooms, bedrooms, price, email } = props;
   return (
     <div className="property-card" data-testid="property-card">
-      <div className="title" data-testid="property-card__title">
+      <div className="card-item" data-testid="property-card__title">
         {title}
       </div>
-      <div className="type-city" data-testid="property-card__type">
+      <div className="card-item" data-testid="property-card__type">
         {type}
       </div>
-      <div className="type-city" data-testid="property-card__city">
+      <div className="card-item" data-testid="property-card__city">
         {city}
       </div>
-      <div className="bathrooms" data-testid="property-card__bathrooms">
+      <div className="card-item" data-testid="property-card__bathrooms">
         Bathrooms: {bathrooms}
       </div>
-      <div className="bedrooms" data-testid="property-card__bedrooms">
+      <div className="card-item" data-testid="property-card__bedrooms">
         Bedrooms: {bedrooms}
       </div>
-      <div className="price" data-testid="property-card__price">
+      <div className="card-item" data-testid="property-card__price">
         £{price}
       </div>
       <div className="email" data-testid="property-card__email">
